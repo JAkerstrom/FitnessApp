b67c9b0b99ef82d05126beb6911e666e17c64d4e
johanna åkerström <johanna_akerstrom@hotmail.com>
2019-04-16 15:56:45 +0200

    redux store for user
    first redux components

---
+++ FitnessApp-master/ClientApp/src/Redux/Reducers/RootReducer.js
@@ -0,0 +1,8 @@
+﻿import { combineReducers } from 'redux';
+import UserReducer from './UserReducer';
+
+const rootReducer = combineReducers({
+    UserReducer: UserReducer
+});
+
+export default rootReducer;
---
+++ FitnessApp-master/ClientApp/src/Redux/Reducers/UserReducer.js
@@ -0,0 +1,18 @@
+﻿export default function UserReducer(state = initialState, action){
+    switch (action.type) {
+        case "CHANGE_USERSTATE":
+            return Object.assign({}, state, {
+                user: action.user,
+                message: action.message,
+                receiver: action.receiver
+            });
+        default:
+            return state;
+    };
+}
+
+const initialState = {
+    user: "",
+    message: "",
+    receiver: 0
+};
