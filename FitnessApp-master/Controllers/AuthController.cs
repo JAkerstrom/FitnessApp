c8294b99dd83dc62e21337358228674fb9eb3149
johanna åkerström <johanna_akerstrom@hotmail.com>
2019-04-24 17:30:48 +0200

    removed unescessary constants

--- FitnessApp-master/Controllers/AuthController.cs
+++ FitnessApp-master/Controllers/AuthController.cs
@@ -9,11 +9,6 @@ namespace fitnessapp.Controllers
     [Route("[controller]/[action]")]
     public class AuthController : Controller
     {
-        private static int LOGIN = 1;
-        private static int REGISTER = 2;
-        private static int UPDATE = 3;
-        private static int DEFAULT = 4;
-
         private UserService _userService;
         private AuthService _authService;
 
