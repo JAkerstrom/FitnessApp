d9fed94b333d6952292043e194a086110c17b0a6
johanna åkerström <johanna_akerstrom@hotmail.com>
2019-04-24 17:32:10 +0200

    -||-

--- FitnessApp-master/Controllers/AccountController.cs
+++ FitnessApp-master/Controllers/AccountController.cs
@@ -10,11 +10,6 @@ namespace fitnessapp.Controllers
     [Route("[controller]/[action]")]
     public class AccountController : Controller
     {
-        private static int LOGIN = 1;
-        private static int REGISTER = 2;
-        private static int UPDATE = 3;
-        private static int DEFAULT = 4;
-
         private UserService _userService;
         private AuthService _authService;
 
